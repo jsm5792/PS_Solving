8a4930de1b8a0cd61474353bd15a60b3e18e5cca
jsm5792 <164445982+jsm5792@users.noreply.github.com>
2026-01-04 15:46:23 +0900

    Update 11659.cpp
    Implement prefix sum array for range queries

--- DP/11659.cpp
+++ DP/11659.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+int N,M;
+int a[100002];
+int b[100002];
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    b[0]=0;
+    for(int i=1;i<=N;i++)
+    {
+        cin >> a[i];
+        b[i] = b[i-1] + a[i];
+    }
+    for(int i=0;i<M;i++)
+    {
+        int x,y;
+        cin >> x >> y;
+        cout << b[y]-b[x-1] << '\n';
+    }
+    
+}
