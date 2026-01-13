32ccee1b28231676dbadd289d604b245b76312e3
jsm5792 <164445982+jsm5792@users.noreply.github.com>
2026-01-13 13:36:18 +0900

    Add files via upload

---
+++ Math/6359.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int N;
+        cin >> N;
+        cout << (int)sqrt(N)<<'\n';
+    }
+}
