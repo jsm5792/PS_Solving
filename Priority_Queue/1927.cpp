728fba3091937ece88e3a10e820a24998b542008
jsm5792 <164445982+jsm5792@users.noreply.github.com>
2026-02-02 15:16:00 +0900

    Add files via upload

---
+++ Priority_Queue/1927.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int N;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    priority_queue<int,vector<int>,greater<int>> pq;
+    cin >> N;
+    while(N--)
+    {
+        int x;
+        cin >> x;
+        if(x==0)
+        {
+            if(pq.empty())
+            {
+                cout << "0\n";
+            }
+            else
+            {
+                cout << pq.top()<<'\n';
+                pq.pop();
+            }
+        }
+        else
+        {
+            pq.push(x);
+        }
+    }
+}
