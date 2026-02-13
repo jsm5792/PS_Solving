048a65995293b4e84055a339070fdd71c04b1900
jsm5792 <164445982+jsm5792@users.noreply.github.com>
2026-02-13 18:31:58 +0900

    Add files via upload

---
+++ Minimum_Spanning_Tree/9372.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int N, M;
+        cin >> N >> M;
+        while(M--)
+        {
+            int a,b;
+            cin >> a >> b;
+        }
+        cout << N-1 <<'\n';
+    }
+}
