80444e5fea13e1a1d09b44b88d42ab3348dad4af
jsm5792 <164445982+jsm5792@users.noreply.github.com>
2026-03-03 20:25:54 +0900

    Add files via upload

---
+++ Backtracking/15666.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N,M;
+int a[13];
+int b[13];
+
+void solve(int x,int y)
+{
+    if(x==M)
+    {
+        for(int i=0;i<M;i++)
+        {
+            cout << b[i] <<' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int temp = 0;
+    for(int i=y;i<N;i++)
+    {
+        if(temp != a[i])
+        {
+            b[x] = a[i];
+            temp = a[i];
+            solve(x+1,i);
+        }
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for(int i=0;i<N;i++)
+    {
+        cin >> a[i];
+    }
+    sort(a,a+N);
+    solve(0,0);
+}
